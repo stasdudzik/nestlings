b9d6b3325181cb2330b4d996985777e241f2bd58
Stas Dudzik <stas.dudzik@gmail.com>
2023-05-15 16:26:43 +0200

    make prisma service extend PrismaClient class to access its methods, create prisma module, import it to auth module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class AuthService {
-  login() {
-    return { msg: 'Hi, you have Logged IN' };
-  }
-
+  constructor(private prisma: PrismaService) {}
   signup() {
     return { msg: 'Hi, you have Signed UP' };
+  }
+
+  login() {
+    return { msg: 'Hi, you have Logged IN' };
   }
 }
