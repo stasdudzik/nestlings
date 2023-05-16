66d5d9b9b892c4ec864e48271c9dbeac01a91da2
Stas Dudzik <stas.dudzik@gmail.com>
2023-05-16 11:55:52 +0200

    create user, restrict select in prisma record

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup(dto: AuthDto) {
-    return { msg: 'Hi, you have Signed UP' };
+  async signup(dto: AuthDto) {
+    //generate the password hash
+    const hash = await argon.hash(dto.password);
+    // save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+      select: {
+        id: true,
+        email: true,
+        createdAt: true,
+      },
+    });
+    // return the saved user
+    return user;
   }
 
   login() {
