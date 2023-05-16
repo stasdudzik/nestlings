a9c6e465b3265d010fd9004878c966dfbc23b61b
Stas Dudzik <stas.dudzik@gmail.com>
2023-05-16 21:43:01 +0200

    clean consol log

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,8 +7,7 @@ import { JwtGuard } from 'src/auth/guard';
 export class UserController {
   // GET /users/me
   @Get('me')
-  getMe(@GetUser() user: User, @GetUser('email') email: string) {
-    console.log({ email });
+  getMe(@GetUser() user: User) {
     return user;
   }
 }
