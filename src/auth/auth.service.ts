23b6e1a1f4341a026fe40da7355c9ed8b200628b
Stas Dudzik <stas.dudzik@gmail.com>
2023-05-16 12:55:33 +0200

    add signin logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
@@ -33,7 +34,25 @@ export class AuthService {
     }
   }
 
-  login() {
-    return { msg: 'Hi, you have Logged IN' };
+  async login(dto: AuthDto) {
+    // find user by mail
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email: dto.email,
+      },
+    });
+    // if user does not exist throw exception
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    // compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    // if incorrect throw exception
+    if (!pwMatches) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+
+    // remove hash and send back user
+    delete user.hash;
+    return user;
   }
 }
