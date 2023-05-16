223ee890a41329f3320518887d1ba515feaf4a69
Stas Dudzik <stas.dudzik@gmail.com>
2023-05-16 21:42:31 +0200

    add get-user decorator for cleaner code

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   // GET /users/me
-  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User, @GetUser('email') email: string) {
+    console.log({ email });
+    return user;
   }
 }
