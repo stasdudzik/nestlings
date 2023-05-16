602e5431df531f0f1ecfe4416e3d5c0455f87ab3
Stas Dudzik <stas.dudzik@gmail.com>
2023-05-16 10:39:30 +0200

    add dto interface, use a barrell export

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   //   POST /auth/signup
   @Post('signup')
-  signup(@Body() dto: any) {
+  signup(@Body() dto: AuthDto) {
     console.log({ dto });
     return this.authService.signup();
   }
