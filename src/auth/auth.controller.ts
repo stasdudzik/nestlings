2f5b594cda449a2571fff5b08c5a13222ad3a7a8
Stas Dudzik <stas.dudzik@gmail.com>
2023-05-16 10:01:35 +0200

    use request object from express in controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   //   POST /auth/signup
   @Post('signup')
-  signup() {
+  signup(@Req() req: Request) {
+    console.log(req.body);
     return this.authService.signup();
   }
 
