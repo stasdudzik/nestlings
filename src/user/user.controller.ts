b3801b60b79fb0a979fe815ca556416dd6930a41
Stas Dudzik <stas.dudzik@gmail.com>
2023-05-16 21:20:42 +0200

    add users/me route and introduce Guards for jwt authorisation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('user')
-export class UserController {}
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
+@Controller('users')
+export class UserController {
+  // GET /users/me
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getMe(@Req() req: Request) {
+    return req.user;
+  }
+}
