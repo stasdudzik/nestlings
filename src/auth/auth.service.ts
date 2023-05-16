05cdcdf2cf05e35e50d91a8db79efdd4c85e6610
Stas Dudzik <stas.dudzik@gmail.com>
2023-05-16 12:49:22 +0200

    add error handling to signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
@@ -8,20 +8,29 @@ export class AuthService {
   async signup(dto: AuthDto) {
     //generate the password hash
     const hash = await argon.hash(dto.password);
-    // save the new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-      select: {
-        id: true,
-        email: true,
-        createdAt: true,
-      },
-    });
-    // return the saved user
-    return user;
+
+    try {
+      // try saving user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Credentials taken');
+      }
+      throw error;
+    }
   }
 
   login() {
