98adda7df08b31f893fe027a07b3944f3337d44f
Stas Dudzik <stas.dudzik@gmail.com>
2023-05-17 16:03:03 +0200

    add bookmarks service, controller and dtos

---
+++ src/bookmark/bookmark.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import {
+  CreateBookmarkDto,
+  EditBookmarkDto,
+} from './dto';
+
+@Injectable()
+export class BookmarkService {
+  getBookmarks(userId: number) {}
+
+  getBookmarkById(
+    userId: number,
+    bookmarkId: number,
+  ) {}
+
+  createBookmark(
+    userId: number,
+    bookmarkId: number,
+    dto: CreateBookmarkDto,
+  ) {}
+
+  editBookmarkById(
+    userId: number,
+    dto: EditBookmarkDto,
+  ) {}
+
+  deleteBookmarkById(
+    userId: number,
+    bookmarkId: number,
+  ) {}
+}
